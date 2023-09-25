91396c332ca81a68df1e3e178b8a14bc68014227
John <ashton.basile@gmail.com>
2023-09-25 11:40:56 +0100

    Having all Flyable animals fly

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,18 @@ public class Main {
         Duck duck1 = new Duck("Donald");
         Eagle eagle1 = new Eagle("Sam");
         Snake snake1 = new Snake("Kaa");
+
+//        duck1.fly();
+//        eagle1.fly();
+//        snake1.swim();
+//        duck1.swim();
+
+        Flyable[] flyingAnimals = new Flyable[2];
+        flyingAnimals[0] = duck1;
+        flyingAnimals[1] = eagle1;
+
+        for (Flyable animal: flyingAnimals) {
+            animal.fly();
+        }
     }
 }
